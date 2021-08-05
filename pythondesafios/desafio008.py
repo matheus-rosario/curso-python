44bbc9c749194b8b4b056e9558b63e1d3f68cc3b
Matheus Rosário <matheusj762@gmail.com>
2021-08-05 11:21:31 -0300

    more exercises

--- pythondesafios/desafio008.py
+++ pythondesafios/desafio008.py
@@ -1,6 +1,4 @@
-#======================================================================#
-#Escreva um programa que leia um valor em metros e o exiba convertido  #
-# em centímetros e milímetros                                          #
-#======================================================================#
+#Escreva um programa que leia um valor em metros e o exiba convertido
+# em centímetros e milímetros 
 n = float(input('Digite o valor em metros: '))
 print(f'{n} metros equivale a {n * 100} centímetros ou {n * 1000} milímetros.')
--- pythondesafios/desafio010.py
+++ pythondesafios/desafio010.py
@@ -1,6 +1,4 @@
-#======================================================================#
-#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira  #
-# e mostre quantos dólares ela pode comprar.                           #
-#======================================================================#
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
+# e mostre quantos dólares ela pode comprar.
 n = float(input('Quanto dinheiro você tem na carteira? '))
 print(f'Com {n} reais você pode comprar {n / 3.27:.2f} dólares')
