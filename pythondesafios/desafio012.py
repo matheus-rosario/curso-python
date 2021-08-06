b73e884615e73aed8ed5edd2690b50da408cd899
Matheus Rosário <matheusj762@gmail.com>
2021-08-05 21:25:23 -0300

    more exercises

--- pythondesafios/desafio012.py
+++ pythondesafios/desafio012.py
@@ -1,4 +1,4 @@
 #Faça um algoritimo que leia o preço de um produto
 #e mostre seu novo preço, com 5% de desconto.
-p = float(input('Qual o preço do produto? '))
-print(f'Na liquidação esse produto tem 5% de desconto, e custa R$ {p * 0.95} reais')
+p = float(input('Qual o preço do produto? R$'))
+print(f'Na liquidação esse produto tem 5% de desconto, e custa R${p * 0.95:.2f} reais')
