7556fdc03b68cfa35a7317559eaf58b11f3c5342
Matheus Rosário <matheusj762@gmail.com>
2021-08-10 18:34:41 -0300

    Update desafio017.py

--- pythondesafios/desafio017.py
+++ pythondesafios/desafio017.py
@@ -2,6 +2,6 @@
 #e do cateto adjacente de um triângulo retângulo, calcule
 #e mostre o comprimento da hipotenusa.
 from math import sqrt
-oposto = float(input('Qual  o comprimento do cateto oposto? '))
+oposto = float(input('Qual o comprimento do cateto oposto? '))
 adjacente = float(input('Qual o comprimento do cateto adjacente? '))
 print(f'O comprimento da hipotenusa é {sqrt((oposto ** 2) + (adjacente ** 2)):.2f}')
