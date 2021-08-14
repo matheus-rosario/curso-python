b5ac5e3a2c54d167de85969f1a4d95d289f5bac3
Matheus Rosário <matheusj762@gmail.com>
2021-08-13 21:42:04 -0300

    more exercises

--- pythondesafios/desafio037.py
+++ pythondesafios/desafio037.py
@@ -2,3 +2,15 @@
 #-1 para binário,
 #-2 para octal e
 #-3 para hexadecimal.
+numero = int(input('Digite um número inteiro: ').strip())
+print('Qual será a base de conversão? ')
+print('  1 - Binário \n  2 - Octal \n  3 - Hexadecimal' )
+conversao = int(input('Base de conversão: ').strip())
+if conversao == 1: 
+  print(f'O número {numero} convertido para binário é: {bin(numero)}')
+elif conversao == 2:
+  print(f'O número {numero} convertido para octal é: {oct(numero)}')
+elif conversao == 3:
+  print(f'O número {numero} convertido para hexadecimal é: {hex(numero)}')
+else:
+  print('Número Invalido')
--- pythondesafios/desafio038.py
+++ pythondesafios/desafio038.py
@@ -1,4 +1,12 @@
-#Esvreva um programa que leia dois números inteiros e compare-os, mostrando na tela a seguinte mensagem:
+#Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela a seguinte mensagem:
 #-O primeiro valor é maior 
 #- O segundo valor é maior
 #- Não existe valor maior, os dois são iguais.
+num1 = int(input('Digite um número: ').strip())
+num2 = int(input('Digite outro número: ').strip())
+if num1 > num2:
+  print('O primeiro valor é maior!')
+elif num2 > num1:
+  print('O segundo valor é maior!')
+else:
+  print('Não existe valor maior, os dois são iguais!')
