4190d41960d7ceb5543abd244dce5aaa0cadc7d9
Matheus Rosário <matheusj762@gmail.com>
2021-08-17 19:46:42 -0300

    Update desafio045.py

--- pythondesafios/desafio045.py
+++ pythondesafios/desafio045.py
@@ -1,1 +1,8 @@
 #Crie um programa que faça o computador jogar Jokenpô com com você.
+from random import randint
+computador = randint(1, 3)
+print('Vamos jogar Jokenpô, escolha sua jogada: \n1 - Pedra \n2 - Papel \n3 - Tesoura')
+jogador = int(input('Sua Jogada: '))
+if jogador == 1 and computador == 1:
+  print(f'Computador jogou Pedra \nVocê jogou Pedra \nEMPATE!')
+#elif jogador == 1 and computador == 2:
