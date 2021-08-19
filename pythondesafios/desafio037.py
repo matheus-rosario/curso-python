da2d848eebd349981bab035495fcbd69ef7a0d2b
Matheus Rosário <matheusj762@gmail.com>
2021-08-19 11:08:22 -0300

    Update desafio037.py

--- pythondesafios/desafio037.py
+++ pythondesafios/desafio037.py
@@ -2,15 +2,15 @@
 #-1 para binário,
 #-2 para octal e
 #-3 para hexadecimal.
-numero = int(input('Digite um número inteiro: ').strip())
+numero = int(input('Digite um número inteiro: '))
 print('Qual será a base de conversão? ')
 print('  1 - Binário \n  2 - Octal \n  3 - Hexadecimal' )
-conversao = int(input('Base de conversão: ').strip())
+conversao = int(input('Base de conversão: '))
 if conversao == 1: 
-  print(f'O número {numero} convertido para binário é: {bin(numero)}')
+  print(f'O número {numero} convertido para binário é: {bin(numero)[2:]}')
 elif conversao == 2:
-  print(f'O número {numero} convertido para octal é: {oct(numero)}')
+  print(f'O número {numero} convertido para octal é: {oct(numero)[2:]}')
 elif conversao == 3:
-  print(f'O número {numero} convertido para hexadecimal é: {hex(numero)}')
+  print(f'O número {numero} convertido para hexadecimal é: {hex(numero)[2:]}')
 else:
   print('Número Invalido')
