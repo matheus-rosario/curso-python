5466dce139fe42eccb2ee19453029feb2d1caa9b
Matheus Rosário <matheusj762@gmail.com>
2021-08-19 11:52:23 -0300

    Update desafio040.py

--- pythondesafios/desafio040.py
+++ pythondesafios/desafio040.py
@@ -8,7 +8,7 @@ nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
 if media < 5.0:
   print('REPROVADO!')
-elif media > 5.0 and media <6.9:
+elif media >= 5.0 and media <=6.9:
   print('RECUPERAÇÃO!')
 else:
   print('APROVADO!')
