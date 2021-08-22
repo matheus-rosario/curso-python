24332360fce82ef8342e841bd46302f05cae4aff
Matheus Rosário <matheusj762@gmail.com>
2021-08-21 22:06:20 -0300

    Update desafio045.py

--- pythondesafios/desafio045.py
+++ pythondesafios/desafio045.py
@@ -1,12 +1,37 @@
 #Crie um programa que faça o computador jogar Jokenpô com com você.
-import random
-pedra = 'pedra'
-papel = 'papel'
-tesoura = 'tesoura'
-computador = random.choice([pedra, papel, tesoura])
-print('Vamos jogar Jokenpô, escolha sua jogada: \n1 - Pedra \n2 - Papel \n3 - Tesoura')
-jogador = int(input('Sua Jogada: '))
-if jogador == computador:
-  print(f'Computador jogou {computador.upper()} \nVocê jogou {jogador} \nEMPATE!')
-else:
-  print('Jogada Inválida')
+from random import randint
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0,2)
+print('Vamos jogar Jokenpô, escolha sua jogada: \n0 - Pedra \n1 - Papel \n2 - Tesoura')
+jogador = int(input('Qual é a sua jogada? '))
+print('-=' * 15)
+print(f'O computador jogou {itens[computador]}')
+print(f'Você jogou {itens[jogador]}')
+if computador == 0:
+  if jogador == 0:
+    print('EMPATOU!')
+  elif jogador == 1:
+    print('VOCÊ VENCEU!')
+  elif jogador == 3:
+    print('O COMPUTADOR VENCEU!')
+  else:
+    print('JOGADA INVÁLIDA!')
+elif computador == 1:
+  if jogador == 0:
+    print('O COMPUTADOR VENCEU!')
+  elif jogador == 1:
+    print('EMPATOU!')
+  elif jogador == 2:
+    print('O JOGADOR VENCEU!')
+  else:
+    print('JOGADA INVÁLIDA')
+elif computador == 2:
+  if jogador == 0:
+    print('O JOGADOR VENCEU!')
+  elif jogador == 1:
+    print('O COMPUTADOR VENCEU!')
+  elif jogador == 2:
+    print('EMPATOU!')
+  else:
+    print('JOGADA INVALIDA!')
+print('-=' * 15)
