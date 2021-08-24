a13ea1f5cfea8c42ed297ab4ba5804f9ee3bc0cc
Matheus Rosário <matheusj762@gmail.com>
2021-08-24 18:20:11 -0300

    Update desafio052.py

--- pythondesafios/desafio052.py
+++ pythondesafios/desafio052.py
@@ -1,2 +1,10 @@
 #Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
 num = int(input('Digite um número: '))
+total = 0
+for c in range(1, num + 1):
+  if num % c == 0:
+    total += 1
+if total == 2:
+  print(f'O numero {num} é primo')
+else:
+  print(f'O número {num} não é primo')
