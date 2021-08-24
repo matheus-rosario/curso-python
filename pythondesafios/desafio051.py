d7492d91789332e9be74c735507e5fac4c888997
Matheus Rosário <matheusj762@gmail.com>
2021-08-24 17:00:29 -0300

    Update desafio051.py

--- pythondesafios/desafio051.py
+++ pythondesafios/desafio051.py
@@ -1,9 +1,6 @@
 #Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
 primeiro = int(input('Digite o primeiro termo: '))
 razão = int(input('Digite a razão: '))
-próximo = primeiro + razão
-print(primeiro)
-print(próximo)
-for c in range(1, 9):
-  próximo += razão
-  print(próximo)
+próximo = primeiro + (10 - 1) * razão
+for c in range(primeiro, próximo, razão):
+  print(c)
