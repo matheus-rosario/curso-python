532ee8a48a7048fc10f4726765cdb11cd782740e
Matheus Rosário <matheusj762@gmail.com>
2021-08-24 08:38:25 -0300

    Update desafio050.py

--- pythondesafios/desafio050.py
+++ pythondesafios/desafio050.py
@@ -1,2 +1,7 @@
 #Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
-n1 = int(input('Digite o primeiro número:'))
+soma = 0
+for c in range(1, 7):
+  num = int(input('Digite um número: '))
+  if num % 2 == 0:
+    soma += num
+print(f'A soma dos números pares é: {soma}')
