a0ba7deb9c91d6f2c58d39d42d8993621e49c005
Matheus Rosário <matheusj762@gmail.com>
2021-08-25 11:04:51 -0300

    Update desafio056.py

--- pythondesafios/desafio056.py
+++ pythondesafios/desafio056.py
@@ -2,3 +2,26 @@
 #No final do programa, mostre:
 #a média de idade do grupo, qual é o nome do homem mais velho
 #e quantas mulheres têm menos de 20 anos.
+somaidade = 0
+media = 0
+maioridade = 0
+nomevelho = ''
+count = 0
+for c in range(1, 5):
+  print(f'=========={c}° Pessoa==========')
+  nome = str(input('Nome: ')).strip()
+  idade = int(input('Idade: '))
+  sexo = str(input('Sexo [M/F]: ')).strip()
+  somaidade += idade
+  if c == 1 and sexo in 'Mm':
+    maioridade = idade
+    nomevelho = nome
+  if sexo in 'Mn' and idade > maioridade:
+    maioridade = idade
+    nomevelho = nome
+  if sexo in 'Ff' and idade < 20:
+    count += 1
+media = somaidade / 4
+print(f'A media de idade é de {media} anos')
+print(f'O homem mais velho é o {nomevelho}, ele tem {maioridade} anos.')
+print(f'{count} mulheres tem menos de 20 anos.')
