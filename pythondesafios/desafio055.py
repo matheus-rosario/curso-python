30bc658869695c2ca75cb24d0ed1ca4b6cc0cb3d
Matheus Rosário <matheusj762@gmail.com>
2021-08-25 10:23:13 -0300

    Update desafio055.py

--- pythondesafios/desafio055.py
+++ pythondesafios/desafio055.py
@@ -1,6 +1,7 @@
 #Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 peso = []
-for c in range(0, 5):
-  novo = float(input('Digite o peso: '))
+for c in range(1, 6):
+  novo = float(input(f'Digite o peso da {c}° pessoa: '))
   peso.append(novo)
-print(f'O maior peso é {max(peso)}')
+print(f'O maior peso lido foi de {max(peso)}Kg')
+print(f'O menor peso lido foi de {min(peso)}Kg')
