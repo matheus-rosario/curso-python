b5c047b778a7c8e32ca15a6ff21a0f1329c8d1bd
Matheus Rosário <matheusj762@gmail.com>
2021-08-24 21:09:20 -0300

    Update desafio053.py

--- pythondesafios/desafio053.py
+++ pythondesafios/desafio053.py
@@ -1,1 +1,11 @@
 #Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for c in range(len(junto) -1, -1, -1):
+  inverso += junto[c]
+if inverso == junto:
+  print('Temos um palíndromo')
+else:
+  print('A frase digitada não é um palíndromo')
