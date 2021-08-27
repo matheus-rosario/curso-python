235b38584d9dbda78f06caa5bd2567f2ef04b1f3
Matheus Rosário <matheusj762@gmail.com>
2021-08-27 15:53:40 -0300

    Update desafio060.py

--- pythondesafios/desafio060.py
+++ pythondesafios/desafio060.py
@@ -1,2 +1,8 @@
 #Faça um programa que leia um número qualquer e mostre o seu fatorial.
 #Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
+from math import factorial
+número = 0
+while número >= 0:
+  número = int(input('Digite um número: '))
+  fatorial = factorial(número)
+  print(f'O fatorial de {número} é {fatorial}')
