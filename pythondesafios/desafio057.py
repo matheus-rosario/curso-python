7bf55a9c304c5dd9d06f5dfc72f0ace64875035e
Matheus Rosário <matheusj762@gmail.com>
2021-08-27 14:44:26 -0300

    Update desafio057.py

--- pythondesafios/desafio057.py
+++ pythondesafios/desafio057.py
@@ -1,1 +1,5 @@
 #Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+sexo = str(input('Digite o sexo [M/F]: ')).upper()
+while sexo != 'M' and sexo != 'F':
+  sexo = str(input('Digite o sexo [M/F]: ')).upper()
+print(f'Seu sexo é: {sexo}')
