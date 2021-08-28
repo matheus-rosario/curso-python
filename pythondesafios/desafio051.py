702d6c1883fce81f8a962f65fd8bc242dba0d026
Matheus Rosário <matheusj762@gmail.com>
2021-08-28 10:57:15 -0300

    updated exercises

--- pythondesafios/desafio051.py
+++ pythondesafios/desafio051.py
@@ -3,4 +3,5 @@ primeiro = int(input('Digite o primeiro termo: '))
 razão = int(input('Digite a razão: '))
 próximo = primeiro + (10 - 1) * razão
 for c in range(primeiro, próximo, razão):
-  print(c)
+  print(c, end= ' -> ')
+print('FIM')
