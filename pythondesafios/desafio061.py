de14a103a7a13a8b0197f427b25104874f9d0c59
Matheus Rosário <matheusj762@gmail.com>
2021-09-02 09:50:33 -0300

    Update desafio061.py

--- pythondesafios/desafio061.py
+++ pythondesafios/desafio061.py
@@ -1,10 +1,9 @@
 # Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
 primeiro = int(input('Digite o primeiro termo: '))
 razão = int(input('Digite a razão: '))
-termos = primeiro + razão
+termos = primeiro
 contador = 1
-print(primeiro, end=' -> ')
-while contador < 10:
+while contador <= 10:
   print(termos, end=' -> ')
   termos += razão
   contador += 1
