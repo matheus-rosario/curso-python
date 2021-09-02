69a8bf0b4d2d671e535c865da0c014e3eb3092d1
Matheus Rosário <matheusj762@gmail.com>
2021-09-02 10:04:13 -0300

    Update desafio062.py

--- pythondesafios/desafio062.py
+++ pythondesafios/desafio062.py
@@ -1,12 +1,16 @@
 #Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.
 primeiro = int(input('Digite o primeiro termo: '))
 razão = int(input('Digite a razão: '))
-termos = primeiro + razão
+termos = primeiro
 contador = 1
-print(primeiro, end=' -> ')
-while contador < 10:
-  print(termos, end=' -> ')
-  termos += razão
-  contador += 1
-  if contador == 10:
-    input('Quantos termos a mais você quer ver? ')
+total = 0
+mais = 10
+while mais != 0:
+  total = total + mais
+  while contador <= total:
+    print(termos, end=' -> ')
+    termos += razão
+    contador += 1
+  print('PAUSA')
+  mais = int(input('Quantos termos a mais você quer ver? '))
+print(f'A progressão terminou com {total} termos mostrados.')
