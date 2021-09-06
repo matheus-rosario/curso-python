0057b3f361c008e5e8e91c08791c5378b1e06316
Matheus Rosário <matheusj762@gmail.com>
2021-09-06 19:39:14 -0300

    Create desafio068.py

---
+++ pythondesafios/desafio068.py
@@ -0,0 +1,46 @@
+#Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+from random import randint
+soma = contador = 0
+resultado = ''
+print('-='*15)
+print('VAMOS JOGAR PAR OU ÍMPAR')
+print('-='*15)
+while True:
+  computador = randint(0, 10)
+  número = int(input('Digite um valor: '))
+  jogador = str(input('Par ou Ímpar? [P/I] ')).strip().upper()
+  soma = computador + número
+  print('-'*15)
+  if soma % 2 == 0:
+    resultado = 'P'
+    print(f'Você jogou {número} e o computador {computador}. Total de {soma} DEU PAR')
+    print('-'*15)
+  else:
+    resultado = 'I'
+    print(f'Você jogou {número} e o computador {computador}. Total de {soma} DEU ÍMPAR')
+    print('-'*15)
+  if resultado == jogador:
+    contador += 1
+    print('VOCÊ VENCEU!')
+    print('Vamos jogar novamente...')
+    print('-='*15)
+  else:
+    print('VOCÊ PERDEU!')
+    print('-='*15)
+    print(f'GAME OVER! Você venceu {contador} vezes.')
+
+
+
+  '''if soma % 2 == 0 and jogador == 'P':
+    print(f'Você jogou {número} e o computador {computador}. Total de {soma} DEU PAR')
+    print('VOCÊ VENCEU')
+    print('-'*15)
+    print('Vamos jogar novamente...')
+  if soma % 2 != 0 and jogador == 'I':
+    print(f'Você jogou {número} e o computador {computador}. Total de {soma} DEU IMPAR')
+    print('VOCÊ VENCEU')
+    print('-'*15)
+    print('Vamos jogar novamente...')
+  else:
+    print(f'Você jogou {número} e o computador {computador}. Total de {soma}')'''
+  
