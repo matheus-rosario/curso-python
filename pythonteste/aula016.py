9259d05aa8d79dd4abc0b0e57f8dee0b840a8326
Matheus Rosário <matheusj762@gmail.com>
2021-09-11 09:24:38 -0300

    Create aula016.py

---
+++ pythonteste/aula016.py
@@ -0,0 +1,7 @@
+lanches = 'Hamburguer', 'Suco', 'Pizza', 'Pudim'
+for comida in lanches:
+  if comida == 'Suco':
+    print(f'Eu vou beber {comida}')
+  else:
+    print(f'Eu vou comer {comida}')
+print('Comi pra caramba!')
